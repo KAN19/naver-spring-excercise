7c02d53b395059ccd949fdb1adee15634eefb1cb
Kiet Nguyen <6393445-19520666NK@users.noreply.gitlab.com>
2022-08-23 13:51:30 +0700

    boliplate code

---
+++ src/main/java/com/ronald/naverexercise/NaverExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.ronald.naverexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaverExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ronald/naverexercise/NaverExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronald.naverexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
